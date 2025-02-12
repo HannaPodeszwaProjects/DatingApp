d59d3a3888bc2450335ab17ec8bcab836f428dfa
hannapodeszwa <hanna.podeszwa@gmail.com>
2025-02-12 14:45:15 +0100

    Dodaj pliki projektów.

---
+++ client/src/app/_modules/shared.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToastrModule } from 'ngx-toastr';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    }),
+    BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
+    NgxSpinnerModule.forRoot({ type: 'ball-climbing-dot' })
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule,
+    TabsModule,
+    NgxSpinnerModule
+  ]
+})
+export class SharedModule { }
